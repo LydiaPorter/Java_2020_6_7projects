9ff174ee676f31567f069b1ded9e629c58118906
LydiaPorter <67480153+LydiaPorter@users.noreply.github.com>
2020-09-10 09:35:07 -0600

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("Lydia");
+		System.out.println("10th Grade!");
+		System.out.println("SHS");
+		System.out.println("print this");
+		System.out.println("Stansbury");
+		System.out.println("LP");
+		System.out.println("print test");
+		System.out.println("third edit");
+		System.out.println("look at this");
+
+	}
+}
